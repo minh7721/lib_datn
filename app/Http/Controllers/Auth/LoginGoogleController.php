2bdc70ee10309e01d4a1009a35d1853558166ad9
minh7721 <nhatminh7721@gmail.com>
2023-06-06 00:57:36 +0700

    Login google

---
+++ app/Http/Controllers/Auth/LoginGoogleController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use DateTime;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class LoginGoogleController extends Controller
+{
+    public function index(){
+        try {
+
+            $user = Socialite::driver('google')->user();
+            $existed = User::where('social_id', $user->id)->first();
+
+            if($existed){
+
+                Auth::login($existed);
+
+                return redirect()->route('document.home.index');
+
+            }else{
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'social_id'=> $user->id,
+                    'avatar' => $user->avatar,
+                    'social_type'=> 'google',
+                    'password' => encrypt('my-google'),
+                    'created_at' => new DateTime(),
+                    'updated_at' => new DateTime(),
+                ]);
+                Auth::login($newUser);
+                return redirect()->route('document.home.index');
+            }
+
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginGoogleController;
 use App\Http\Controllers\Frontend\DocumentController;
 use App\Http\Controllers\Frontend\LoginController;
+use Laravel\Socialite\Facades\Socialite;
 
 //use App\Http\Controllers\ViewerController;
 //use Illuminate\Support\Facades\Route;
@@ -31,6 +33,24 @@ Route::get('auth/login', [LoginController::class, 'getLogin'])->name('frontend.a
 Route::post('auth/login/post', [LoginController::class, 'postLogin'])->name('frontend.auth.postLogin');
 Route::get('logout', [LoginController::class, 'logout'])->name('frontend.auth.logout');
 
+// Login Facebook
+Route::get('auth/facebook', function () {
+    return Socialite::driver('facebook')->redirect();
+})->name('frontend.login.facebook');
+
+Route::get('auth/facebook/callback', function () {
+    return 'Call back Facebook';
+});
+
+// Google
+Route::get('auth/google', function () {
+    return Socialite::driver('google')->redirect();
+})->name('frontend.login.google');
+
+Route::get('auth/google/callback', [LoginGoogleController::class, 'index']);
+
+
+
 Route::get('/document', function () {
     return view('frontend_v4.pages.document.detail');
 });
@@ -39,13 +59,8 @@ Route::get('/home', function () {
     return view('frontend_v4.pages.home.home');
 });
 
-//Route::get('/document', function () {
-//    return view('frontend_v4.pages.document.detail');
-//});
+
 Route::get('/document/{slug}', [DocumentController::class, 'view'])->name('document.detail');
-//Route::get('/search', function (){
-//    return view('frontend_v4.pages.search.search');
-//});
 
 Route::get('/search', [DocumentController::class, 'search'])->name('document.search');
 
