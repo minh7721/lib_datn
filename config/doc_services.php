4789f30bcfe3fcddab332402b8427360764d870b
minh7721 <nhatminh7721@gmail.com>
2023-06-26 16:23:25 +0700

    config ai_figure_detector

--- config/doc_services.php
+++ config/doc_services.php
@@ -6,4 +6,8 @@ return [
         'port' => env('TIKA_PORT', 9998),
         'timeout' => env('TIKA_TIMEOUT', 15),
     ],
+    'ai_figure_detector' => [
+        'host' => env('AI_FIGURE_DETECTOR_HOST', '118.70.13.36:6050'),
+        'token' => env('AI_FIGURE_DETECTOR_TOKEN', '123dok2120')
+    ],
 ];
