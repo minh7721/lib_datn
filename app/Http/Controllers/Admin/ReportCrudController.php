f31d59afb9e0fc1daa212c7581af49105f9442fe
minh7721 <nhatminh7721@gmail.com>
2023-06-28 22:58:12 +0700

    update

--- app/Http/Controllers/Admin/ReportCrudController.php
+++ app/Http/Controllers/Admin/ReportCrudController.php
@@ -14,7 +14,6 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 class ReportCrudController extends CrudController
 {
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
@@ -32,16 +31,11 @@ class ReportCrudController extends CrudController
         $this->crud->setFromDb();
     }
 
-    protected function setupCreateOperation()
+    protected function setupUpdateOperation()
     {
         $this->crud->setValidation(ReportRequest::class);
 
         // TODO: remove setFromDb() and manually define Fields
         $this->crud->setFromDb();
-    }
-
-    protected function setupUpdateOperation()
-    {
-        $this->setupCreateOperation();
     }
 }
